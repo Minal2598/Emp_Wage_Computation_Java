fea611182f77cacf64432beee97dc813a0eff65b
Minal <patilminalp@gmail.com>
2021-06-15 20:49:46 +0530

    cal Empwage using switch case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,38 +1,35 @@
+import java.util.Scanner;
 public class EmpWageComputation{
-	public static void main(String[] args){
+	public static void main(String [] args) {
+		int empCheck = 1;
+		int WagePerHr = 20;
+		int FullDayHr = 8;
+		int PartTimeHr =4; 
 		
-		int isFullTime=1;
-		int isPartTime=2;
-		//int empHr;
-		int WagePerHr=20;
-		int FullDayHr=8;
-		int PartTimeHr=4;
-		
-		double empCheck = Math.floor(Math.random()*10)%3;
-		if(empCheck == isFullTime)
-		{
-			System.out.println("Employee is Present");
-			int DailyEmpWage=WagePerHr*FullDayHr;
-			System.out.println("DailyEmpWage of FullTime Employee::"+DailyEmpWage);
-			
-			
-		}
-		else if(empCheck == isPartTime)
-		{
-			System.out.println("Employee is Present");
 
-			int DailyEmpWage=WagePerHr*PartTimeHr;
-			System.out.println("DailyEmpWage of PartTime Employee::"+DailyEmpWage);
-		}else
-		{
-			System.out.println("Employee is Abscent");
-			//empHr=0;
-
-			
-		}
 		
+		double value = Math.floor(Math.random() * 10) % 2;
 		
+		if(value == empCheck) {
+			System.out.println("Employee is Present");
+			Scanner sc = new Scanner (System.in);
+		System.out.println("please Enter 1 for FullTimeEmp or Enter 2 for PartTimeEmp");
+		int n = sc.nextInt();
+		sc.close();
+			switch(n){
+		case 1: 
 		
-	}
-	
+		System.out.println(" Daily Wage of FullTime Employee  is: " +WagePerHr*FullDayHr);
+		break;
+		case 2:
+		System.out.println("Daily Wage of PartTime Employee is: " +WagePerHr*PartTimeHr);
+			break;
+			default:
+			System.out.println("please select only between 1 and 2");
+			}
+			}
+		else 
+		System.out.println("Employee is Absent");
+		
+}
 }
