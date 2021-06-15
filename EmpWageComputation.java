82278f35dec9a2adf0b89819d4199c8bfac0323d
Minal <patilminalp@gmail.com>
2021-06-15 20:28:30 +0530

    cal PartTime Emp & Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,30 +3,33 @@ public class EmpWageComputation{
 		
 		int isFullTime=1;
 		int isPartTime=2;
-		int empHr;
+		//int empHr;
 		int WagePerHr=20;
 		int FullDayHr=8;
+		int PartTimeHr=4;
 		
 		double empCheck = Math.floor(Math.random()*10)%3;
 		if(empCheck == isFullTime)
 		{
 			System.out.println("Employee is Present");
-			empHr=8;
+			int DailyEmpWage=WagePerHr*FullDayHr;
+			System.out.println("DailyEmpWage of FullTime Employee::"+DailyEmpWage);
 			
 			
 		}
 		else if(empCheck == isPartTime)
 		{
-			System.out.println("Employee is Abscent");
-			empHr=4;
+			System.out.println("Employee is Present");
+			int DailyEmpWage=WagePerHr*PartTimeHr;
+			System.out.println("DailyEmpWage of PartTime Employee::"+DailyEmpWage);
 		}else
 		{
-			empHr=0;
+			System.out.println("Employee is Abscent");
+			//empHr=0;
 			
 		}
 		
-		int DailyEmpWage=WagePerHr*empHr;
-		System.out.println("DailyEmpWage::"+DailyEmpWage);
+		
 		
 	}
 	
