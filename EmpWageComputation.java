88b538096b901ba20a20c7c87c7863aa8b6ac6b6
Minal <patilminalp@gmail.com>
2021-06-15 14:31:52 +0530

    cal DailyEmpWage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,10 +2,16 @@ public class EmpWageComputation{
 	public static void main(String[] args){
 		
 		int isFullTime=1;
+		int WagePerHr=20;
+		int FullDayHr=8;
+		
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if(empCheck == isFullTime)
 		{
 			System.out.println("Employee is Present");
+			int DailyEmpWage=WagePerHr*FullDayHr;
+			System.out.println("DailyEmpWage::"+DailyEmpWage);
+			
 		}
 		else
 		{
