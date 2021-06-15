357e844bb1a949780bc5821617809ce0555a78cb
Minal <patilminalp@gmail.com>
2021-06-15 14:09:48 +0530

    Present or Abscent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,16 @@
 public class EmpWageComputation{
 	public static void main(String[] args){
 		
-		System.out.println("Welcome to Employee Wage Computation");
+		int isFullTime=1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == isFullTime)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Abscent");
+		}
 		
 	}
 	
