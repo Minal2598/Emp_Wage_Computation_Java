7e0e6b16f1c13165c91f1b8de0d575a0a0a74266
Minal <patilminalp@gmail.com>
2021-06-23 05:16:18 +0530

    Added:Utility class in employee wage problem

---
+++ Utility.java
@@ -0,0 +1,13 @@
+
+
+public class Utility {
+
+    public static double getRandomDouble() {
+        return Math.floor(Math.random() * 10) % 3;
+    }
+
+    public static int getRandomInt() {
+        return (int)(Math.random() * 10) % 3;
+    }
+
+}
